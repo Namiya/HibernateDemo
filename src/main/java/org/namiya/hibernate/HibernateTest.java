558e21f122604284b6d2d7789bca1e67f8682c48
Namiya <Namiya.Yuuji@gmail.com>
2014-07-01 13:14:28 -0400

    Saving Object by using Hibernate API: If you are using Hibernate's proprietary API, you'll need the hibernate.cfg.xml. If you are using JPA i.e. Hibernate EntityManager, you'll need the persistence.xml.

---
+++ src/main/java/org/namiya/hibernate/HibernateTest.java
@@ -0,0 +1,34 @@
+package org.namiya.hibernate;
+
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.namiya.dto.UserDetails;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		UserDetails user= new UserDetails();
+		user.setUserId(1);
+		user.setUserName("First User");
+		
+		//for In Hibernate 4.3
+		SessionFactory sessionFactory;
+		ServiceRegistry serviceRegistry;
+		Configuration configuration = new Configuration();
+	    configuration.configure();
+	    serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
+	            configuration.getProperties()).build();
+	    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+		
+	    Session session = sessionFactory.openSession();
+	    session.beginTransaction();
+	    session.save(user);
+	    session.getTransaction().commit();
+
+	}
+
+}
