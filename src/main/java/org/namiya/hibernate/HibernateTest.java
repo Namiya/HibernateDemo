5754d3cf574037807ec5759f8036584d16b4fc5f
Namiya <Namiya.Yuuji@gmail.com>
2014-07-02 11:37:40 -0400

    code clearing

--- src/main/java/org/namiya/hibernate/HibernateTest.java
+++ src/main/java/org/namiya/hibernate/HibernateTest.java
@@ -6,28 +6,11 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.namiya.dto.FourWheeler;
-import org.namiya.dto.TwoWheeler;
-import org.namiya.dto.UserDetails;
-import org.namiya.dto.Vehicle;
 
 public class HibernateTest {
 
 	public static void main(String[] args) {
-		UserDetails user= new UserDetails();
-		user.setUserName("First User");
-
-		Vehicle vehicle = new Vehicle();
-		vehicle.setVehicleName("Car");
-	
 		
-		TwoWheeler bike = new TwoWheeler();
-		bike.setVehicleName("Bike");
-		bike.setSteeringHandle("Bike Steering Handle");
-		
-		FourWheeler car = new FourWheeler();
-		car.setVehicleName("Porche");
-		car.setStreeingWheel("Porche Steering Wheel");
 		
 		//for In Hibernate 4.3
 		SessionFactory sessionFactory;
@@ -40,9 +23,6 @@ public class HibernateTest {
 		
 	    Session session = sessionFactory.openSession();
 	    session.beginTransaction();
-	    session.save(vehicle);
-	    session.save(bike);
-	    session.save(car);
 	    session.getTransaction().commit();
 	    session.close();
 	}
