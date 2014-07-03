19ff7c5202fa96668834739b2cc453d99fcb236b
Namiya <Namiya.Yuuji@gmail.com>
2014-07-03 16:27:04 -0400

    implementing HQL and the Query Object

--- src/main/java/org/namiya/hibernate/HibernateTest.java
+++ src/main/java/org/namiya/hibernate/HibernateTest.java
@@ -1,19 +1,18 @@
 package org.namiya.hibernate;
 
 
+import java.util.List;
+
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
-import org.namiya.dto.UserDetails;
 
 public class HibernateTest {
 
 	public static void main(String[] args) {
-		
-		UserDetails user = new UserDetails();
-		user.setUserName("Test User");
 		
 		//for In Hibernate 4.3
 		SessionFactory sessionFactory;
@@ -26,10 +25,14 @@ public class HibernateTest {
 		
 	    Session session = sessionFactory.openSession();
 	    session.beginTransaction();
-		
+	    
+	    Query query = session.createQuery("from UserDetails where userId > 5");
+	    List users = query.list();
+	    
 		session.getTransaction().commit();
 	    session.close();
 	    
+	    System.out.println("Size of List result = " + users.size());
 	}
 
 }
