11cef5463c8f1c8b0562ab1a907770095c89d05c
Namiya <Namiya.Yuuji@gmail.com>
2014-07-03 22:20:46 -0400

    code cleanning

--- src/main/java/org/namiya/hibernate/HibernateTest.java
+++ src/main/java/org/namiya/hibernate/HibernateTest.java
@@ -1,19 +1,11 @@
 package org.namiya.hibernate;
 
 
-import java.util.List;
-
-import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.criterion.Example;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Projections;
-import org.hibernate.criterion.Restrictions;
 import org.hibernate.service.ServiceRegistry;
-import org.namiya.dto.UserDetails;
 
 public class HibernateTest {
 
@@ -31,22 +23,12 @@ public class HibernateTest {
 	    Session session = sessionFactory.openSession();
 	    session.beginTransaction();
 	    
-	    UserDetails exampleUser = new UserDetails();
-	    exampleUser.setUserId(5);
-	    exampleUser.setUserName("five user");
-	    Example example = Example.create(exampleUser).excludeProperty("userName");
-/*	    Criteria criteria = session.createCriteria(UserDetails.class)
-	    						   .setProjection(Projections.property("UserId"));*/  						  
-	    Criteria criteria = session.createCriteria(UserDetails.class)
-				   					.add(example);
-
-	    List<UserDetails> users = (List<UserDetails>) criteria.list();
+	
 	    
 		session.getTransaction().commit();
 	    session.close();
 	    
-	    for (UserDetails u: users)
-	    	System.out.println(u.getUserName());
+
 	    
 	    
 	}
